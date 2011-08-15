e6730bd5146b3ec1900b8d79577858fd203fd47e
Kevin Hollingsworth <kehollingsworth@yahoo.com>
2011-08-15 00:30:40 -0400

    initial file checkin

---
+++ ride_hub.rb
@@ -0,0 +1,213 @@
+# google_maps_link = "http://maps.google.com/maps?saddr=Turkey+Hill+-+Eagleville%4040.1594,-75.4084&daddr=Rt.+73%4040.3003,-75.526+to:Rt.+100%4040.3763,-75.6118+to:Orchard+Rd%4040.3776,-75.6619+to:Mountain+Mary+Rd%4040.3953,-75.6921+to:Fleetwood+Rd%4040.4644,-75.7543+to:Dryville+Rd%4040.4634,-75.7985+to:Scheetz+-+STOP%4040.4853,-75.8385+to:Rt.+143%4040.5457,-75.8804+to:Mountain+Rd%4040.5802,-75.9158+to:Port+Clinton+Ave%4040.5714,-75.9961+to:Rt.+901%4040.6613,-76.2231+to:Turkey+Hill+-+STOP%4040.7635,-76.3829+to:High+Rd%4040.7643,-76.3861+to:Rt.+4024%4040.7523,-76.4413+to:Rt.+125%4040.7527,-76.535+to:Main+St%4040.6089,-76.4498+to:Buddy%27s+Log+Cabin+-+LUNCH+STOP%4040.5663,-76.4007+to:Rt.+645%4040.5227,-76.3856+to:Rt.+645%4040.4188,-76.3194+to:Krumstown+Rd%4040.3468,-76.2776+to:S.+Fort+Zellers+Rd%4040.3413,-76.2127+to:Turkey+Hill+-+STOP%4040.2451,-75.9991+to:Rt.+724%4040.2964,-75.9324+to:Wawa+Royersford+at+422%4040.1913,-75.5302&hl=en&dirflg=ht&z=9&geocode=FajIZAId8FuB-w%3BFQzvZgIdkJB_-w%3BFewXaAIdaEF--w%3BFQAdaAIdtH19-w%3BFSRiaAIdvAd9-w%3BFRBwaQIdxBR8-w%3BFShsaQIdHGh7-w%3BFbTBaQId3Mt6-w%3BFaStagIdMCh6-w%3BFWg0awId6J15-w%3BFQgSawIdPGR4-w%3BFTRxbAIdhO10-w%3BFWwAbgIdTH1y-w%3BFYwDbgIdzHBy-w%3BFazUbQIdLJlx-w%3BFTzWbQIdKCtw-w%3BFYSkawId-Hdx-w%3BFRz-agIdxDdy-w%3BFcxTagIdwHJy-w%3BFfC9aAIdWHVz-w%3BFbCkZwIdoBh0-w%3BFTSPZwIdJBZ1-w%3BFWwXZgIdhFh4-w%3BFdDfZgIdEF15-w%3BFURFZQIdKIB_-w"
+# google_maps_link = "http://maps.google.com/?saddr=40.15943,-75.40842&daddr=40.33536,-75.49129+to:40.37630,-75.61177+to:40.37757,-75.66194+to:40.39528,-75.69214+to:40.46444,-75.75428+to:40.46336,-75.79848+to:40.48071,-75.82480+to:40.48532,-75.83847+to:40.50215,-75.87797+to:40.66131,-76.22311+to:40.77696,-76.46438+to:40.79311,-76.55049+to:40.78759,-76.55872+to:40.60887,-76.44985+to:40.56633,-76.40068+to:40.52270,-76.38565+to:40.41877,-76.31938+to:40.24758,-76.51752+to:40.16854,-76.39956+to:40.09744,-76.29770+to:40.03864,-76.08965+to:40.02537,-75.83271+to:40.13550,-75.80966+to:40.19129,-75.53022&hl=en&dirflg=h&om=1&geocode=FcbIZAId3FuB-w%3BFQB4ZwIdJhiA-w%3BFewXaAIdhkF--w%3BFeIcaAIdjH19-w%3BFRBiaAIdlAd9-w%3BFThwaQId2BR8-w%3BFQBsaQIdMGh7-w%3BFcavaQIdYAF7-w%3BFcjBaQId-st6-w%3BFYYDagIdrjF6-w%3BFT5xbAIdeu10-w%3BFQA1bgIdBD9x-w%3BFRZ0bgIdpu5v-w%3BFYZebgIdgM5v-w%3BFWakawIdxndx-w%3BFTr-agId2Ddy-w%3BFcxTagIdjnJy-w%3BFdK9aAIdbHVz-w%3BFRwhZgIdcG9w-w%3BFVzsZAIdODxy-w%3BFaDWYwIdHMpz-w%3BFfDwYgIdzvZ2-w%3BFRq9YgIdeuJ6-w%3BFUxrZAIdhDx7-w%3BFTpFZQIdFIB_-w&z=9"
+# google_maps_link = "http://maps.google.com/maps?f=d&source=s_d&saddr=Fort+Washington,+PA+19034&daddr=40.18918,-75.10992+to:40.2194939,-75.0763129+to:40.2746503,-75.0223225+to:40.3025221,-75.0477201+to:40.3237043,-75.0260413+to:40.3540239,-75.0077931+to:40.4047838,-75.0696173+to:40.4206759,-75.0967961+to:40.4501651,-75.1213028+to:40.48836,-75.11003+to:40.47805,-75.15545+to:40.559112,-75.2246989+to:40.6011923,-75.2583419+to:40.6182392,-75.320469+to:555+Union+Boulevard,+Allentown,+PA+18109-3229&geocode=FXN3ZAIdo6uE-ykxxJLX5brGiTHmB9zq3PmKcA%3BFfw8ZQId4OmF-ynJWPN4q6_GiTHz7_L2GI4SZg%3BFWWzZQIdKG2G-yl7IO30sK7GiTEtY17zOde7Og%3BFdqKZgIdDkCH-ylBo4ScWqrGiTEQZolzo6A3Dw%3BFbr3ZgId2NyG-ynpEsFfCKrGiTFgMIsPYERaFg%3BFXhKZwIdhzGH-yk9rftBsQHEiTHzHuuAUnQbYA%3BFefAZwIdz3iH-ymxu_w75ADEiTHbj7-VYOJjvw%3BFS-HaAIdT4eG-ynPLywuGATEiTHALU8gQO8Oeg%3BFUPFaAIdJB2G-ymrAzWnWgTEiTEGBEtkO33eTQ%3BFXU4aQIdar2F-ynB9CWdEgXEiTFmUqg3TKVJew%3BFajNaQIdcumF-ylN8QRvng_EiTEi8Tr6KvePZA%3BFWKlaQIdBjiF-ymV2POoqhrEiTFGY9tdgv8rhw%3BFQjiagIdhimE-ynZMmljXRHEiTEeUkOw_m0rNw%3BFWiGawIdG6aD-yk5JhV5cBTEiTGuPrBgRLX_Hg%3BFf_IawIda7OC-ymfF8pEnhXEiTFBMOSqHGNFRw%3BFbDiawIdC7GA-yEWKkm-jZ90OSnvZPhmPDnEiTG1a_f-DI_nsA&hl=en&mra=mrv&via=1,2,3,4,5,6,7,8,9,10,11,12,13,14&sll=40.239702,-75.039454&sspn=0.038722,0.086946&ie=UTF8&ll=40.244681,-75.048294&spn=0.077437,0.173893&z=13"
+# google_maps_link = "http://maps.google.com/maps?saddr=1242+Goodman+Drive,+Fort+Washington,+PA&daddr=40.1440939,-75.2041204+to:40.29605,-74.78232+to:40.48915,-74.69222+to:40.542321,-74.638224+to:43.603384,-73.2606559+to:43.6584022,-73.2307123+to:Fair+Haven,+Vermont+05743+(Bomoseen+State+Park)&hl=en&sll=43.654506,-73.23216&sspn=0.011054,0.022831&geocode=FSOlZAId5bSE-ykdOCDRxLrGiTGfnjHP3I1WOA%3BFd2MZAId6HmE-ynP8nWlH7vGiTHw3eC9QXjpkg%3BFXLeZgIdkOmK-yn3ygFlM_3DiTH8Egz6IpzCZQ%3BFb7QaQIdhEmM-ylr9Z4mrO7DiTEX3PS5wAG87w%3BFXGgagIdcByN-ykDF_1N4erDiTH_LQH-tey6mw%3BFbhVmQIdkSGi-ylL405EDhDgiTEP5uqdzQ6nbg%3BFaIsmgIdiJai-ykRY2YQChvgiTFufudyx5iNMQ%3BFYkrmgIdtZai-yFXpAj4_BPaBQ&mra=dpe&mrsp=6&sz=16&via=1,2,3,4,5,6&t=h&z=16"=
+google_maps_link = "http://maps.google.com/maps?saddr=1242+Goodman+Drive,+Fort+Washington,+PA&daddr=40.0051398,-75.3822591+to:39.9997319,-75.3882648+to:39.9967298,-75.405749+to:39.9908811,-75.4191491+to:39.98152,-75.507645+to:39.94274,-75.51457+to:39.918024,-75.5377289+to:39.879449,-75.599591+to:39.86159,-75.63756+to:39.80887,-75.70964+to:39.76045,-75.84106+to:39.8395899,-75.9922649+to:39.8157722,-76.1267302+to:39.8308944,-76.2708598+to:PA-372+W%2FHoltwood+Rd+to:39.822356,-76.353497+to:39.844986,-76.385367+to:39.9174643,-76.4452439+to:40.0328091,-76.5311558+to:40.082045,-76.6899979+to:40.175592,-76.795337+to:40.254279,-76.8873789+to:Championship+Way,+Harrisburg,+Pennsylvania+17101+(City+Island+Parking+Lot)&hl=en&sll=39.839123,-76.242199&sspn=0.092135,0.202389&geocode=FSOlZAId5bSE-ykdOCDRxLrGiTGfnjHP3I1WOA%3BFRNuYgIdDcKB-ykz1wIr4erGiTH1VhCa5FDs3Q%3BFfNYYgIdmKqB-ynTtWxDHevGiTEXXRmigPjlRg%3BFTlNYgIdS2aB-yn9ewwXDOvGiTGIxcKVPE06rA%3BFWE2YgId8zGB-ymvFBAOnevGiTGH1yFscKdjeA%3BFdARYgIdQ9h_-ykVnePk6u3GiTHnG8oUySi6tg%3BFVR6YQIdNr1_-yljMabCF-7GiTHQjlTtTI3Syw%3BFcgZYQIdwGJ_-ymzZdiSL_DGiTH-gkTWSe3t5w%3BFRmDYAIdGXF--ykN6ut6E_rGiTG94f_tR4igxA%3BFVY9YAIdyNx9-ykZjvFMnPnGiTGCXQ1OBDb_NA%3BFWZvXwIdOMN8-ykpJmlCXv_GiTHquL1oEY7yAg%3BFUKyXgId3MF6-ynrs36izazHiTH9k343pDxK5Q%3BFWXnXwIdOHN4-ympmdJwdUzGiTH9k3ceUZhKOA%3BFVyKXwId9mV2-ynn6XdgIjXGiTGqIWdwyLbQpg%3BFW7FXwId9TJ0-ymxasgpoTLGiTGjw3bhoNe1OA%3BFTJ7XwId-lBz-w%3BFRSkXwIdJ_By-ym_n4GHUtPHiTGkGvZjHdXk9g%3BFXr8XwIdqXNy-yklS08rRyvGiTHSmbzCFjgn9Q%3BFZgXYQIdxYlx-ylV6bCnAirGiTHbBUlFAbgHpg%3BFSnaYgIdLTpw-ykRnV8DlJzIiTEdgvH9sIrJHQ%3BFX2aYwIds81t-yk11jxD45DIiTFOHEpq-3ScxA%3BFegHZQIdNzJs-ynzdwPOE5XIiTHJBgdaU7p4GA%3BFUc7ZgIdrspq-ymdMN8XCcHIiTHU6WrJUO4Zrg%3BFS49ZgIdF8lq-yGzZ28Vic1okA&mra=dpe&mrsp=14&sz=13&via=1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22&t=h&z=13"
+itn_route = "-7514281|+4015676|100 Welsh Rd, Horsham, PA 19090|4|
+-7518506|+4016068|1000-1004 Highland Ave, Fort Washington, PA 19034, USA|0|
+-7521780|+4012258|644-670 E Skippack Pike, Fort Washington, PA 19034, USA|0|
+-7536125|+4010502|200-298 Schuylkill River Rd, Bridgeport, PA 19405, USA|0|
+-7571628|+4004837|1150-1152 Hopewell Rd, Downingtown, PA 19335, USA|0|
+-7587473|+4005407|700-754 State Highway 340, Coatesville, PA 19320, USA|0|
+-7597734|+3998798|701 State Highway 41, Gap, PA 17527, USA|0|
+-7601749|+3998660|5300-5498 Futer St, Gap, PA 17527, USA|0|
+-7611519|+3998386|590-598 Strasburg Rd, Paradise, PA 17562, USA|0|
+-7619783|+4002070|Bob Evans Restaurant|2|
+"
+
+class Ride
+  
+  def initialize(source_text)
+    @source_route = SourceRoute.new(source_text)
+    import
+  end
+  
+  def import
+    source_klass = inspect
+    @route = source_klass.new(@source_route)
+    # @route.waypoints.each { |w| puts w.to_s }
+  end
+  
+  def inspect
+    klass = nil
+    klass = GoogleMapsRoute.dup if @source_route.is_google_link?
+    klass = ItnRoute.dup if @source_route.is_itn?
+    klass
+  end
+  
+  def to_navigon
+    output = "navigonUSA-PA://route/?"
+    @route.waypoints.each do |w|
+      output << "target=coordinate//"
+      output << w.longitude.to_s
+      output << "/"
+      output << w.latitude.to_s
+      output << "&"
+    end
+    puts output[0..-2]
+  end
+  
+end
+
+class ItnRoute
+  
+  attr_accessor :waypoints
+  
+  def initialize(source_route)
+    @source_route = source_route
+    import
+  end
+  
+  def import
+    @waypoints = []
+    @source_route.source_text.each_line do |w|
+      @waypoints << ItnWaypoint.new(w)
+    end    
+  end
+  
+end
+
+
+class GoogleMapsRoute
+  
+  attr_accessor :waypoints
+  
+  def initialize(source_route)
+    @link = source_route.source_text
+    import
+  end
+  
+  def import
+    @waypoints = []
+    link_split = @link.split("+to:")
+    add_waypoint find_marker :type => 'start', :text  => link_split[0]
+    add_waypoint find_marker :type => 'destination', :text  => link_split[0]
+    link_split.each { |w| add_waypoint(w) }
+  end
+  
+  def find_marker(args)
+    case args[:type]
+      when 'start'
+        label = 'saddr='
+      when 'destination'
+        label = 'daddr='
+      else
+        label = nil
+    end
+    text = args[:text]
+    index = text.index(label)
+    text[index+6..-1]
+  end
+  
+  def add_waypoint(w)
+    waypoint = GoogleWaypoint.new(w)
+    @waypoints << waypoint if waypoint.is_valid?
+  end
+  
+end
+
+class ItnWaypoint
+
+  attr_accessor :latitude, :longitude
+
+  def initialize(waypoint)
+    @waypoint = waypoint.split('|')
+    find_lat_and_long
+  end
+  
+  def find_lat_and_long
+    @latitude = Geocode.new(@waypoint[1].insert(3, "."))
+    @longitude = Geocode.new(@waypoint[0].insert(3, "."))
+  end
+  
+end
+
+
+class GoogleWaypoint
+  
+  attr_accessor :latitude, :longitude, :description
+
+  def initialize(waypoint)
+    @waypoint = waypoint
+	if description_included? then
+	  w_split = waypoint.split('%40')
+	  @description = w_split[0]
+	  @coordinates = w_split[1]
+	else
+	  @coordinates = waypoint
+	end
+    find_lat_and_long
+  end
+  
+  def description_included?
+    @waypoint.include?("%40")
+  end
+  
+  def find_lat_and_long
+    c_split = @coordinates.split(',')
+    @latitude = Geocode.new(c_split[0])
+    @longitude = Geocode.new(c_split[1])
+  end
+  
+  def to_s
+    "latitude: #{@latitude.to_s} longitude: #{@longitude.to_s}"
+  end
+  
+  def is_valid?
+    @latitude.is_valid? and @longitude.is_valid?
+  end
+
+end
+
+class Geocode
+  
+  def initialize(val)
+    @val = val.to_f
+  end
+  
+  def is_valid?
+    @val > -180 && @val < 180 && @val != 0
+  end
+  
+  def to_s
+    @val.to_s
+  end
+  
+end
+
+class SourceRoute
+  
+  attr_accessor :source_text
+  
+  def initialize(source_text)
+    @source_text = source_text
+  end
+  
+  def is_itn?
+    # itn file format - http://www.gpspassion.com/forumsen/topic.asp?TOPIC_ID=39107
+    @source_text.each_line do |w|
+      w_split = w.split("|")
+      return false unless w_split.length == 5
+      return false unless ["0", "1", "2", "3", "4"].include?(w_split[3]) 
+    end
+    true 
+  end
+  
+  def is_google_link?
+    @source_text.match(/maps.google.com/)
+  end
+  
+end
+
+r = Ride.new(google_maps_link)
+r.to_navigon
+
+
+#split = google_maps_link.split("+to:")
+#split.each { |s| puts s }
+
+#i=0
+#s.each_byte { |b| puts "count: #{i += 1}  ascii: #{b}  char: #{b.chr}"  }
+#puts s[s.index('%40')+3..-1 ]
+  
+
